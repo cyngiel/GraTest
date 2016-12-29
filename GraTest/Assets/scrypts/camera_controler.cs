b45fc81defe49d8ea3e68fc145af79dfb5680296
cyngiel <pstefanski07@gmail.com>
2016-12-29 19:13:45 +0100

    start

---
+++ GraTest/Assets/scrypts/camera_controler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_controler : MonoBehaviour {
+
+    public Transform sfera;
+
+		// Update is called once per frame
+	void FixedUpdate () {
+
+        Rigidbody rigidbody = sfera.GetComponent<Rigidbody>();
+
+        Vector3 vector = new Vector3(4f, -6f, -6f);
+        float predkosc = rigidbody.velocity.sqrMagnitude;
+        vector = (1F + predkosc/25) * vector;
+
+        Vector3 newPosition = sfera.position - (vector);
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, 0.05f);
+        transform.LookAt(sfera);
+	}
+}
---
+++ GraTest/Assets/scrypts/sphere_controler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sphere_controler : MonoBehaviour {
+
+    public float minY;
+    void FixedUpdate () {
+        float speed = 10;
+        
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+        rigidbody.isKinematic = false;
+       
+
+        if (Input.GetKey(KeyCode.UpArrow)){
+            rigidbody.AddTorque(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidbody.AddTorque(-Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody.AddTorque(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody.AddTorque(-Vector3.forward * speed);
+        }
+        minY = transform.position.y;
+       
+
+        if (minY < -10)
+        {
+            rigidbody.isKinematic = true;
+            transform.position = new Vector3(0f, 1.5f, 0f);
+        }
+    }
+}
