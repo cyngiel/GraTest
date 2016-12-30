675210fac3fd36dfb40f257092075261ccc38544
AconLight <w.mosieski@interia.pl>
2016-12-30 20:00:42 +0100

    próba zmiany pozycji

---
+++ GraTest/Assets/scrypts/wojtas' scipts/player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+    //Vector3 velocity, acceleration;
+    //public float startAcc;
+	// Use this for initialization
+	void Start () {
+        //velocity = new Vector3();
+        //acceleration = new Vector3(startAcc, 0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //vectorAdd(velocity, acceleration, Time.deltaTime);
+        //vectorAdd(transform.position, velocity, Time.deltaTime);
+        //transform.position.Set(0, 0, transform.position.z + 1);
+
+        transform.position.Set(0, 4, 150); // co jest z tym nie tak?
+    }
+
+    void vectorAdd(Vector3 v1, Vector3 v2, float scl)
+    {
+        v1.Set(v1.x + v2.x * scl, v1.y + v2.y * scl, v1.z + v2.z * scl);
+    }
+
+}
